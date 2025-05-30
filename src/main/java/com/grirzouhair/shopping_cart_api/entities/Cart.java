502b0954579f2a7747da718943de04cacf113cc1
Grir_Zouhiar <zouhairgrir937@gmail.com>
2025-05-30 16:03:39 +0100

    creating entities

---
+++ src/main/java/com/grirzouhair/shopping_cart_api/entities/Cart.java
@@ -0,0 +1,27 @@
+package com.grirzouhair.shopping_cart_api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "created_date", insertable = false, updatable = false)
+    private LocalDate createdDate;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+}
---
+++ src/main/java/com/grirzouhair/shopping_cart_api/entities/Product.java
@@ -0,0 +1,30 @@
+package com.grirzouhair.shopping_cart_api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+}
